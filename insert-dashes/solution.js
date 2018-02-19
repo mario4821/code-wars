432d3432ad89bd59a87679299740ae61184e36ab
Mario Flores Jr <m.flores4821@gmail.com>
2018-02-18 17:23:33 -0800

    solution

---
+++ insert-dashes/solution.js
@@ -0,0 +1,19 @@
+function insertDash(num) { 
+
+    var prev='', 
+        newstring='';
+    
+        num = num.toString();
+        
+        for (let i = 0; i < num.length; i++){
+            parseInt(num[i]) % 2 == 0 ? current='even' : current='odd';
+            if (current == 'odd' && prev == 'odd'){
+                newstring = newstring + '-' + num[i];
+                prev = 'odd';
+            } else {
+                newstring = newstring + num [i];
+                 prev = current;
+            }
+        }
+      return newstring;   
+    }    
